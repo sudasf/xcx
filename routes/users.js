d3b73f17a236b41daae2238a477680b6dd6f58f7
sudasf <1518223884@qq.com>
2016-11-08 15:30:28 +0800

    change

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ var {User}=require('../lib/db')
 var {checkSession}=require('../lib/mySession')
 
 /* GET users listing. */
-router.get('/update',checkSession, function(req, res, next) {
+router.post('/update',checkSession, function(req, res, next) {
    var {userInfo,nickName,avatarUrl,gender,province,city,country}=req.body
    var updateData={
    	 userInfo,nickName,avatarUrl,gender,province,city,country
    }
    var myUser=req.myUser
+   console.log(myUser);
    myUser.userInfo=updateData
    myUser.save((err)=>{
    	    if(err){
