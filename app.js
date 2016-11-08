6436243071b96b3065cb09a361020187dc9a5c4e
sudasf <1518223884@qq.com>
2016-11-08 21:08:00 +0800

    change

--- app.js
+++ app.js
@@ -81,17 +81,19 @@ http.listen(3008, function(){
 
 // });
 
-var WebSocket = require('ws');
-var ws = new WebSocket('ws://www.91jiayun.com/socket');
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({ server: http  })
 
-ws.on('open', function open() {
+wss.on('connection', function connection(ws) {
+  var location = url.parse(ws.upgradeReq.url, true);
+  // you might use location.query.access_token to authenticate or share sessions
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+  console.log('连上了');
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
   ws.send('something');
-});
-
-ws.on('message', function(data, flags) {
-  // flags.binary will be set if a binary data is received.
-  // flags.masked will be set if the data was masked.
-  console.log(data);
 });
 
 
